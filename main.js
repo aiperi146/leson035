0e31297d0f2fb02d76e05ea2bb52bc86102ca12a
Aiperi Anarbekovna <ajperikamcybekova146@gmail.com>
2024-02-23 13:34:39 +0600

    div

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const firstObject = {
     name: "FirstObject",
-   
+    printName: printName 
 };
+
 
 const secondObject = {
     name: "SecondObject",
-    
+     printName: printName
 };
+
+function printName() {
+    console.log(this.name);
+}
+
+firstObject.printName();
+secondObject.printName();
